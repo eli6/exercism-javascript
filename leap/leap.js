f4eb7fc6c555d1a9f2008fc3094802437c78cf3b
Elin Fritiofsson <elfr1702@student.miun.se>
2018-12-08 17:10:35 +0100

    added test for leap year

---
+++ leap/leap.js
@@ -0,0 +1,13 @@
+export function isLeap(year) {
+
+    //year is evenly divisible by 4, could be a leap year
+    if(year%4 == 0) {
+
+        //return false if year is evenly divisible by 100 but not by 400, in that case it's not a leap year.
+        //otherwise return true.
+        return (year % 100 == 0 && year % 400 != 0) ? false : true;
+
+
+    }
+
+}
