7cb4bf18f77f23df0ac8de72400d9f2cb403573d
Elin Fritiofsson <elfr1702@student.miun.se>
2018-12-17 18:31:40 +0100

    added Points and Radius objects

--- darts/darts.js
+++ darts/darts.js
@@ -1,4 +1,26 @@
 /**
+ * Points for different circles in the 'Dart' game.
+ * @type {{outer: number, middle: number, inner: number, none: number}}
+ */
+const Points = {
+    outer: 1,
+    middle: 5,
+    inner: 10,
+    none: 0
+};
+
+/**
+ * Radius from Origin (0,0) of the concentric circles in the 'Dart' game.
+ * @type {{outer: number, middle: number, inner: number}}
+ */
+const Radius = {
+    outer: 10,
+    middle: 5,
+    inner: 1
+};
+
+
+/**
  * Function to calculate points on the target in a "Dart" game.
  * Hit in outer circle gives 10 points. Hit in the middle circle gives 5 points and hit in the inner circle, 1 point.
  * @param x X coordinate where the dart has hit.
@@ -11,18 +33,17 @@ export function solve(x, y) {
         return null;
     }
 
-    const Outerradius = 10;
-    const Middleradius = 5;
-    const InnerRadius = 1;
+    //determine distance from center
+    const dist = Math.sqrt(x**2 + y**2);
 
     //if we are inside of the target, check if we are also inside middle or inner target, otherwise return 1.
-    if(x <= Outerradius || y <= Outerradius ){
-        if( x <= Middleradius && y <= Middleradius ){
+    if(dist <= Radius.outer ){
+        if(dist <= Radius.middle ){
             //return 10 if we are in the inner circle, if we're "only" in the middle one, return 5.
-            return (x <= InnerRadius && y <= InnerRadius) ? 10 : 5;
+            return (dist <= Radius.inner) ? Points.inner : Points.middle;
         }
-        return 1;
+        return Points.outer;
     }
     //return 0 if we are outside of the target.
-    return 0;
+    return Points.none;
 }
