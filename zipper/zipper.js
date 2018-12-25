f84649b61f3b778e523af51d7d60f78288ae736d
Elin Fritiofsson <elfr1702@student.miun.se>
2018-12-25 10:24:22 +0100

    6 first tests pass. added up() support

--- zipper/zipper.js
+++ zipper/zipper.js
@@ -1,31 +1,39 @@
 
 class Zipper {
 
-    constructor(tree, crumbs){
+    constructor(tree, crumbs, root){
         this.tree = tree;
-        this.crumbs = [];
+        this.root = root;
+        this.crumbs =[];
+        this.crumbs = crumbs;
+        //todo perhaps push last parent before creating new zipper instead?
     }
 
     static fromTree(tree) {
-       return new Zipper(tree);
+        let history = [];
+        history.push(tree);
+       return new Zipper(tree, history, tree);
     }
 
     toTree() {
-        return this.tree;
+       // return this.tree;
+       return this.root;
     }
 
    //var =  bt( 1, bt(2, null, leaf(3)), leaf(4) );
 
     left(){
         let parent = this.tree;
+        this.crumbs.push(parent); //add this one to history.
         //refactor to clear up...
-        let zipper = new Zipper(this.tree[Object.keys(this.tree)[1]], parent);
+        let zipper = new Zipper(this.tree[Object.keys(this.tree)[1]], this.crumbs, this.root);
         return zipper["tree"] == null ? null : zipper;
     }
 
     right() {
         let parent = this.tree;
-        let zipper = new Zipper(this.tree[Object.keys(this.tree)[2]], parent);
+        this.crumbs.push(parent); //add this one to history.
+        let zipper = new Zipper(this.tree[Object.keys(this.tree)[2]], this.crumbs, this.root);
         return zipper["tree"] == null ? null : zipper;
 
     }
@@ -33,6 +41,15 @@ class Zipper {
     value(){ // ja?
         //get first parameter
         return this.tree[Object.keys(this.tree)[0]];
+    }
+
+    up(){
+        if(this.crumbs.length > 1){
+            return new Zipper(this.crumbs[this.crumbs.length-1], this.crumbs.splice(-1,1), this.root);
+        } else {
+            return null; //otherwise we dont have any higher level, we're at the top.
+        }
+
     }
 }
 
